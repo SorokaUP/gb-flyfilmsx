55df2f1f765bcadff3f66413a21086d93f6b3e8e
SorokaUP <51316654+SorokaUP@users.noreply.github.com>
2021-06-26 15:39:03 +0300

    На карточку фильма добавлены постеры

--- app/src/main/java/ru/sorokin/flyfilmsx/model/Film.kt
+++ app/src/main/java/ru/sorokin/flyfilmsx/model/Film.kt
@@ -1,13 +1,16 @@
 package ru.sorokin.flyfilmsx.model
 
-import java.io.Serializable
 import java.util.*
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Film(
     val id: Int,
     val caption: String,
     val description: String,
     val dateFrom: Date,
     val tags: String,
-    val posterPath: String
-    ) : Serializable
+    val posterPath: String,
+    val rate:Float
+    ) : Parcelable
