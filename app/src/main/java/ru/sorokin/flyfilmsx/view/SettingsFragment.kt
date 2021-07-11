e1d11ead2d1a6eb6f8fc5d1f946c148a73aa392a
SorokaUP <51316654+SorokaUP@users.noreply.github.com>
2021-07-11 15:50:01 +0300

    SharedPreference для настроек Контента для взрослых

--- app/src/main/java/ru/sorokin/flyfilmsx/view/SettingsFragment.kt
+++ app/src/main/java/ru/sorokin/flyfilmsx/view/SettingsFragment.kt
@@ -1,13 +1,21 @@
 package ru.sorokin.flyfilmsx.view
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.sorokin.flyfilmsx.R
+import android.widget.CompoundButton
+import androidx.fragment.app.Fragment
+import ru.sorokin.flyfilmsx.databinding.FragmentSettingsBinding
+
+
+private const val IS_SHOW_18_PLUS = "SHOW18PLUS"
 
 class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,8 +24,24 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.isContent18plus.isChecked = false
+        activity?.let {
+            binding.isContent18plus.isChecked = (it.getPreferences(Context.MODE_PRIVATE).getBoolean(IS_SHOW_18_PLUS, false))
+        }
+        binding.isContent18plus.setOnCheckedChangeListener { buttonView, isChecked ->
+            activity?.let {
+                with(it.getPreferences(Context.MODE_PRIVATE).edit()) {
+                    putBoolean(IS_SHOW_18_PLUS, isChecked)
+                    apply()
+                }
+            }
+
+        }
     }
 
     companion object {
