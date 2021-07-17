bf1278fbee7658a4affe200408e0af507b1239f7
SorokaUP <51316654+SorokaUP@users.noreply.github.com>
2021-07-17 23:08:07 +0300

    Добавлена кнопка на экран списка фильмов. Добавлены разрешения. Добавлен вывод инфомрации о местоположении.

--- app/src/main/java/ru/sorokin/flyfilmsx/view/MainActivity.kt
+++ app/src/main/java/ru/sorokin/flyfilmsx/view/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import ru.sorokin.flyfilmsx.R
 import ru.sorokin.flyfilmsx.databinding.AppBarMainBinding
 import ru.sorokin.flyfilmsx.databinding.MainActivityBinding
