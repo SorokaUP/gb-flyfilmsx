811c01f6a5768a09c8e7d8eaebd8bd252155f181
SorokaUP <51316654+SorokaUP@users.noreply.github.com>
2021-07-17 23:28:53 +0300

    Добавлен фрагмент с картой. Получен API ключ Google Maps. Однако проваливается авторизация.

--- app/src/main/java/ru/sorokin/flyfilmsx/view/MainActivity.kt
+++ app/src/main/java/ru/sorokin/flyfilmsx/view/MainActivity.kt
@@ -93,6 +93,16 @@ class MainActivity : AppCompatActivity() {
 
                 true
             }
+            R.id.menu_google_maps -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, MapsFragment())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+
+                true
+            }
             else -> {
                 super.onOptionsItemSelected(item)
             }
